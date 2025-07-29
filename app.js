5897abcbafd269f1d6b124808a6f845f50b37d24
nldcrafter <nldcrafter1111@gmail.com>
2025-07-29 06:40:35 +0630

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 import { renderWallet } from './components/wallet.js';
+import { renderDex } from './components/dex.js';
+import { renderFutures } from './components/futures.js';
 
 window.addEventListener('DOMContentLoaded', () => {
-  renderWallet(); // shows wallet when site loads
+  renderWallet(); // default load
 });
+
+window.loadWallet = () => renderWallet();
+window.loadDex = () => renderDex();
+window.loadFutures = () => renderFutures();
