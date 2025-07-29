db9b440218917572391806a6918c031a8b2cfd98
nldcrafter <nldcrafter1111@gmail.com>
2025-07-29 06:42:05 +0630

    Update futures.js

--- Components/futures.js
+++ Components/futures.js
@@ -1,1 +1,8 @@
-
+export function renderFutures() {
+  document.getElementById('main').innerHTML = `
+    <div class="futures-card">
+      <h1>📈 Futures Trading</h1>
+      <p>Coming soon: Trade with leverage on AVODA DEX.</p>
+    </div>
+  `;
+}
